576ca32cd49a18aa3c4522676cba9e21156a2861
Tu Nguyen <miamorwest@gmail.com>
2020-02-02 16:10:13 +0700

    02.02.20

--- filter.py
+++ filter.py
@@ -1,8 +1,10 @@
 import os
 import shutil
 
+path = '/media/tunguyen/Devs/Dataset/cuckoo/malware'
 with open('malware_report_A_Dung.txt', 'r') as f:
-    for line in f:
-        filename = line
-        if os.path.exists('../Dataset/cuckoo/'+filename):
-            shutil.move('../Dataset/cuckoo/'+filename, '../Dataset/cuckoo_split/'+filename)
+    lines = [line.rstrip() for line in f]
+
+    for filename in lines:
+        if os.path.exists(path+'/'+filename):
+            shutil.move(path+'/'+filename, '/media/tunguyen/Devs/Dataset/cuckoo_split/classified/malware/'+filename)
